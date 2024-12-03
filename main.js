d2f9d9beeeb80f3e16eada4edd6b344140273dfc
nonnsweety <age2380@gmail.com>
2024-12-03 19:42:51 +0500

    add finish registor window

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-function inputButton(set){
-  var setting = set.value
-  if (setting == "admin")
-    window.location.href = "admin.html"
-}
+var array_user = ["admin", "user", "other"]
+var array_password = ["admin", "user", "other"]
 
 function filterCards(category) {
     const cards = document.querySelectorAll('.card');
@@ -11,14 +8,36 @@ function filterCards(category) {
             card.style.display = 'block';
         } else {
             card.style.display = 'none';
-        }
+        };
     });
-}
-filterCards('all')
+};
+filterCards('all');
 
 
 function registor(){
-    window.location.href = "registor.html"
-}
+    window.location.href = "registor.html";
+};
 
+function checkForm(element) {
+    var name = element.name.value;
+    var pass = element.pass.value;
+    var return_pass = element.return_pass.value;
+    
+    if (name == "" || pass == "" || return_pass == "") {
+        
+        alert("not work")        
+
+    } else {
+        for (var i = 0; i < array_user.length; i++){
+            if (name == array_user[i] && pass == array_password[i] && return_pass == array_password[i]) {
+                alert("yes")
+                break
+            } else {
+                alert("no")
+            }
+        }
+    }
+
+    return false;
+};
 
