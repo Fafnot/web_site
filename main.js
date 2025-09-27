a8c7dc55884ab3846b92ed4c7433d30c62d505fc
nonnsweety <age2380@gmail.com>
2025-09-27 16:02:30 +0500

    add material

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
+function updateClock() {
+    let now = new Date();
+    let hours = String(now.getHours()).padStart(2, '0');
+    let minutes = String(now.getMinutes()).padStart(2, '0');
+    let seconds = String(now.getSeconds()).padStart(2, '0');
+    let timeString = `${hours}:${minutes}:${seconds}`;
+
+    // обновляем все элементы с классом .clock
+    document.querySelectorAll('.clock').forEach(el => {
+      el.textContent = timeString;
+    });
+  }
+
+setInterval(updateClock, 1000);
+updateClock(); // сразу запустить
+
+
+
 
 
